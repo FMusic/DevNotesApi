eef9ae72ae988e3fc781310760a7d2ce73e86676
FMusic <frane.music@gmail.com>
2020-11-13 16:02:30 +0100

    deps for openapi added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+
+	id("com.github.johnrengelman.processes") version "0.5.0"
+	id("org.springdoc.openapi-gradle-plugin") version "1.3.0"
 	war
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
